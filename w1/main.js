a3fea5942792b1b440f639814c001e045f10e12c
Kairi Her <k82604h@gmail.com>
2024-10-06 17:44:31 -0700

    added w1

--- w1/main.js
+++ w1/main.js
@@ -1,26 +1,2 @@
-console.log('Hello from inside the main.js file')
-
-//let myVar;
-const myVar = null;
-const myVarType = typeof(myVar);
-console.log("myVarType" + myVarType);
-console.log('myVarType ${myVarType}');
-
-if(myVarType === "string") {
-    console.log('this line will not run')
-} else {
-    console.log('this line will run')
-} 
-
-function runNow () {
-    if(myVarType === "number") {
-        console.log('will 11 this one run?')
-    } else {
-        console.log('will 13 this one run?')
-}}
-
-runNow();
-runNow();
-
-// myVar can be a number also
+console.log('Hello from inside')
 
