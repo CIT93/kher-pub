bc9fafdbba83f63ece34599a3577eee516aa4463
Kairi Her <k82604h@gmail.com>
2024-11-11 20:23:56 -0800

    coding done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,14 @@
-console.log('Yo')
+console.log('Yo');
+
+const myHouseMembers = 2;
+const mySizeHome = 4;
+const myFoodChoice = 10;
+const myWaterComsumption = 1;
+const myHouseholdPurchases = 8;
+const myWasteProduce = 40;
+const myRecycleWaste = 12;
+const myAnnualTransport = 0;
+const cfpTotal = myHouseMembers + mySizeHome + myFoodChoice + myWaterComsumption + myHouseholdPurchases + myWasteProduce + myRecycleWaste + myAnnualTransport
+
+const myFootPrint = document.querySelector("h2");
+myFootPrint.textContent = "77";
