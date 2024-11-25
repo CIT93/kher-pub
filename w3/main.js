54b0e888c2d1fef5dadd6168272be06b03141b28
Kairi Her <k82604h@gmail.com>
2024-11-24 23:35:27 -0800

    I watched the output video that fixed the code

--- w3/main.js
+++ w3/main.js
@@ -1,3 +1,15 @@
+function determineHouseSizePts(size) {
+  if(size === 'large') {
+    cardonFootprintPoints = cardonFootprintPoints + 10
+  } else if (size === 'medium') {
+    cardonFootprintPoints = cardonFootprintPoints + 7
+  } else if (size === 'small'){
+    cardonFootprintPoints = cardonFootprintPoints + 4
+  } else if (size === 'apt') {
+    cardonFootprintPoints = cardonFootprintPoints + 2
+  }
+}
+
 function determineHouseholdPoints(numberInHousehold) {
   console.log("inside the function");
   if (numberInHousehold === 1) {
@@ -21,29 +33,8 @@ function determineHouseholdPoints(numberInHousehold) {
 }
 
 let cardonFootprintPoints = 0;
-// const numberInHousehold = 7;
+
 //global scope
 
 determineHouseholdPoints(3);
-determineHouseholdPoints(4);
-
-// my try
-function determineSizeOfHome(sizeOfHome) {
-    console.log("inside the funct");
-    if (sizeOfHome === 10) {
-        carbonFootprintPoints = sizeOfHome + but;
-    } else if (sizeOfHome === 7) {
-        carbonFootprintPoints = sizeOfHome + 7;
-    } else if (sizeOfHome === 4) {
-        carbonFootprintPoints = sizeOfHome + 4;
-    } else if (sizeOfHome > 2) {
-        carbonFootprintPoints = sizeOfHome + 2;
-    }
-    console.log(`Based on the number of size of the household of ${sizeOfHome} the resul would be ${carbonFootprintPoints}.`);
-}
-let carbonFootprintPoints = 0;
-
-determineSizeOfHome(4);
-determineSizeOfHome(10);
-
-// I got the code to run but I wanted it to say Large,Medium,Small and Apartment but It kept saying 'not defined'. I couldn't quite figure it out on why it was doing that so I stopped. I got working code, but I didn't quite get the code I wanted.
+determineHouseSizePts("medium");
